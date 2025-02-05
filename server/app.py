8298c7e465897ceda562239e4142d6203ae94726
Bill Nguyen <bill.nguyen@nutanix.com>
2025-02-05 14:12:47 +1100

    fixx

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ from ollama_parse import models as ollama_models_dict
 import base64
 import io
 import ollama
-import subprocess
+import os
 
 # Before we do anything, make sure all the models are downloaded
 print("BOOTING UP")
@@ -34,17 +34,7 @@ for model in ollama_models_dict:
     if not found:
         print(f"haven't been downloaded...downloading:")
         try:
-            process = subprocess.Popen(
-                ["ollama", "pull", model['ollama_name']],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True
-            )
-            for line in process.stdout:
-                print(line.strip())
-            for line in process.stderr:
-                print(line.strip())
-            process.wait()
+            os.system(f"ollama pull {model['ollama_name']}")
         except Exception as e:
             print(f"{str(e)}")
 
