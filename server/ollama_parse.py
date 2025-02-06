3ceba491d9d8271cb40bcaa579d190dabc29f137
Bill Nguyen <bill.nguyen@nutanix.com>
2025-02-06 15:30:22 +1100

    sneaky deepseek

--- server/ollama_parse.py
+++ server/ollama_parse.py
@@ -17,17 +17,17 @@ models = [
         "ollama_name": "llama3.3",
     },
     {
-        "display_name": "DeepSeek-R1-Distill-Qwen-14B",
+        "display_name": "DS-R1-Distill-Qwen-14B",
         "note": "Experimental | Fast",
         "ollama_name": "deepseek-r1:14b",
     },
     {
-        "display_name": "DeepSeek-R1-Distill-Qwen-32B",
+        "display_name": "DS-R1-Distill-Qwen-32B",
         "note": "Experimental | Slow",
         "ollama_name": "deepseek-r1:32b",
     },
     {
-        "display_name": "DeepSeek-R1-Distill-Llama-70B",
+        "display_name": "DS-R1-Distill-Llama-70B",
         "note": "Experimental | Very slow",
         "ollama_name": "deepseek-r1:70b",
     }
