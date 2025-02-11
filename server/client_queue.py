4a79088d79c090dfe6cdabba3f221b9223f8452a
Bill Nguyen <bill.nguyen@nutanix.com>
2025-02-11 13:57:16 +1100

    debug BE

--- server/client_queue.py
+++ server/client_queue.py
@@ -6,11 +6,13 @@
 client_queue = []
 # Internal functions
 # Internal functions
-from flask_socketio import SocketIO, emit
+from flask_socketio import emit
+from flask import current_app as app
 
 def refresh_queue_to_all():
     for i, client_state in enumerate(client_queue):
         sid = client_state["sid"]
+        app.logger.debug(f"sending queue pos of {str(i)} back to {sid}")
         emit("queue", {"queue_pos": i}, to=sid)
 
 def dequeue():
