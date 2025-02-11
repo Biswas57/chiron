68cfa377909c37c929a3e8576b7b606e6bb76555
Bill Nguyen <bill.nguyen@nutanix.com>
2025-02-11 14:30:03 +1100

    aaaA?????

--- server/client_queue_mgmt.py
+++ server/client_queue_mgmt.py
@@ -10,12 +10,14 @@ from flask import current_app as app
 client_queue = []
 
 def refresh_queue_to_all():
+    global client_queue
+
     app.logger.debug("refresh_queue_to_all")
     app.logger.debug(client_queue)
     for i, client_state in enumerate(client_queue):
         sid = client_state["sid"]
         app.logger.debug(f"sending queue pos of {str(i)} back to {sid}")
-        emit("queue", {"queue_pos": i}, to=sid)
+        emit("queue", {"queue_pos": i + 1}, to=sid)
 
 def dequeue():
     global client_queue
