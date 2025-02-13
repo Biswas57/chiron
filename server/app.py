b04813967387e4b160c1fa27ba107181f36b1521
AdibAkbari <adib.akbari95@gmail.com>
2025-02-13 15:08:40 +1100

    fix

--- server/app.py
+++ server/app.py
@@ -45,7 +45,7 @@ def download_models():
                         pulled_gbytes = progress.completed / 1000 / 1000 / 1000
                         needed_gbytes = progress.total / 1000 / 1000 / 1000
                         percent = (pulled_gbytes / needed_gbytes) * 100
-                        msg = f"{i + 1}/{len(ollama_models_dict)}: Pulling {model['ollama_name']}, {pulled_gbytes:.2f}MB / {needed_gbytes:.2f}MB = {percent:.2f}%"
+                        msg = f"{i + 1}/{len(ollama_models_dict)}: Pulling {model['ollama_name']}, {pulled_gbytes:.2f}GB / {needed_gbytes:.2f}GB = {percent:.2f}%"
                         app.logger.debug(msg)
                         emit("progress", {"message": msg})
                     else:
